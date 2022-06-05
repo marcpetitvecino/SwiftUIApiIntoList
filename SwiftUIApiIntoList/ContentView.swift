8ff1030f7749b0e061fd3978fef478f92d3a56b2
marcpetitvecino <marcpetitvecino@gmail.com>
2022-06-05 14:11:54 +0200

    Initial Commit

---
+++ SwiftUIApiIntoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApiIntoList
+//
+//  Created by Marc Petit Vecino on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApiIntoList/SwiftUIApiIntoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiIntoListApp.swift
+//  SwiftUIApiIntoList
+//
+//  Created by Marc Petit Vecino on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiIntoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

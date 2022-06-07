f826af73590196eda76179803e16d57b0bde75a4
marcpetitvecino <marcpetitvecino@gmail.com>
2022-06-07 15:21:05 +0200

    Project finished

---
+++ SwiftUIApiIntoList/ViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  ViewModel.swift
+//  SwiftUIApiIntoList
+//
+//  Created by Marc Petit Vecino on 6/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var courses: [Course] = []
+    func fetch() {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, _, error
+            in guard let data = data, error == nil else {
+                return
+            }
+            
+            // Convert to JSON
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
